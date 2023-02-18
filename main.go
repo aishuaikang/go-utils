74faad3968b9fc88edd563f52078efde7c463ede
Ai Shuaikang <2506377990@qq.com>
2023-02-18 19:45:48 +0800

    feat(all): 新增通过窗口句柄获取窗口RECT

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ var (
 	_SpeedInit           = utils.NewProc("SpeedInit")
 	_GetWindowCapture    = utils.NewProc("GetWindowCapture")
 	_GetHwndByTitle      = utils.NewProc("GetHwndByTitle")
+	_GetWindowRect       = utils.NewProc("GetWindowRect")
 )
 
 // 获取鼠标当前移动速度
@@ -107,6 +108,18 @@ func GetWindowCapture(hwnd, x, y, width, height int32, suffix string) (data []by
 func GetHwndByTitle(title string) (hwnd int32) {
 	if code, _, _ := _GetHwndByTitle.Call(uintptr(unsafe.Pointer(&title)), uintptr(unsafe.Pointer(&hwnd))); code != 0 {
 		panic(errors.New("GetHwndByTitle call failed"))
+	}
+	return
+}
+
+type Rect struct {
+	Left, Top, Right, Bottom int32
+}
+
+// 通过窗口标题获取窗口句柄
+func GetWindowRect(hwnd int32) (rect Rect) {
+	if code, _, _ := _GetWindowRect.Call(uintptr(hwnd), uintptr(unsafe.Pointer(&rect.Left)), uintptr(unsafe.Pointer(&rect.Top)), uintptr(unsafe.Pointer(&rect.Right)), uintptr(unsafe.Pointer(&rect.Bottom))); code != 0 {
+		panic(errors.New("GetWindowRect call failed"))
 	}
 	return
 }
