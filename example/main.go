63df320f1bd63942a679bdd96775550d9f241d17
Ai Shuaikang <2506377990@qq.com>
2023-03-03 21:06:30 +0800

    🐞 fix(GDI): 修复截图无法捕获桌面问题

--- example/main.go
+++ example/main.go
@@ -7,83 +7,103 @@ import (
 	"github.com/aishuaikang/go-utils"
 )
 
-func TryCatch(callback func(), catch func(err any)) {
-	defer func() {
-		if err := recover(); err != nil {
-			catch(err)
-		}
-	}()
-	callback()
-}
-
 func main() {
 	log.SetFlags(log.Ltime | log.Lmsgprefix | log.Llongfile | log.Lshortfile)
-	TryCatch(
-		func() {
-			var hwnd int32
 
-			// 启动线程监控窗口
-			go func() {
-				for {
-					newHwnd := utils.GetHwndByTitle("v2rayN - V3.29 - 2020/12/05")
-					if hwnd != newHwnd {
-						hwnd = newHwnd
-						utils.CaptureInit(hwnd, 0, 0, 640, 640)
-					}
-					time.Sleep(1 * time.Second)
-				}
-			}()
+	var hwnd int32
 
-			defer utils.CaptureRelease()
-
-			// 计算屏幕中心点
-			width, height := utils.GetScreenResolution()
-			centerX := int(width / 2)
-			centerY := int(height / 2)
-
-			// 初始化算法（可以重复调用更新参数）
-			utils.SpeedInit(100, 0.2)
-			utils.PidInit(0.1, 0.002, 0.003)
-			utils.LinearInit(0.1)
-			utils.MagneticInit(0.3)
-
-			// for循环模拟推理
-			for {
-				// 判断窗口是否存在
-				if hwnd <= 0 || utils.IsWindowIconic(hwnd) || !utils.IsWindowVisible(hwnd) {
-					log.Println("窗口不存在或最小化或不可见")
-					time.Sleep(1 * time.Second)
-					continue
-				}
-				TryCatch(
-					func() {
-						// 截图
-						startTime := time.Now()
-						utils.CaptureBitmap()
-						captureTime := time.Since(startTime)
-
-						// 获取当前鼠标位置
-						currentX, currentY := utils.GetCursorPos()
-
-						// 调用算法
-						outputX, outputY := utils.Compute("LinearCompute", currentX, currentY, float64(centerX), float64(centerY))
-
-						// 鼠标移动
-						// utils.MoveMouse(outputX, outputY)
-						log.Printf("当前位置：(%2d,%2d) - 截图耗时：%5v\n", int(outputX), int(outputY), captureTime)
-						// utils.BitmapSaveBMP("test.bmp")
-					},
-					func(err any) {
-						log.Println(err)
-						time.Sleep(1 * time.Second)
-					},
-				)
-
+	// 启动线程监控窗口
+	go func() {
+		for {
+			newHwnd, err := utils.GetHwndByTitle("v2rayN - V3.29 - 2020/12/05")
+			if err != nil {
+				log.Println(err)
+				break
 			}
-		},
-		func(err any) {
+			if hwnd != newHwnd {
+				hwnd = newHwnd
+
+				winRect, err := utils.GetWindowRect(hwnd)
+				if err != nil {
+					log.Println(err)
+					break
+				}
+
+				winCenterX := winRect.Width / 2
+				winCenterY := winRect.Height / 2
+
+				err = utils.CaptureInit(0, winCenterX-320, winCenterY-320, 640, 640)
+				if err != nil {
+					log.Println(err)
+					break
+				}
+			}
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	defer utils.CaptureRelease()
+
+	// 计算屏幕中心点
+	width, height, err := utils.GetScreenResolution()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	centerX := int(width / 2)
+	centerY := int(height / 2)
+
+	// 初始化算法（可以重复调用更新参数）
+	utils.SpeedInit(80, 0.1)
+	utils.PidInit(0.1, 0.002, 0.003)
+	utils.LinearInit(0.1)
+	utils.MagneticInit(0.3)
+
+	// for循环模拟推理
+	for {
+		// 判断窗口是否存在
+		if hwnd <= 0 || utils.IsWindowIconic(hwnd) || !utils.IsWindowVisible(hwnd) {
+			log.Println("窗口不存在或最小化或不可见")
+			time.Sleep(1 * time.Second)
+			continue
+		}
+
+		// 截图
+		var data []byte
+
+		startTime := time.Now()
+		utils.CaptureBitmap(&data)
+		captureTime := time.Since(startTime)
+
+		// 获取当前鼠标位置
+		currentX, currentY, err := utils.GetCursorPos()
+		if err != nil {
 			log.Println(err)
-		},
-	)
+			return
+		}
+
+		// 调用算法
+		outputX, outputY, err := utils.Compute(utils.Pid, currentX, currentY, float64(centerX), float64(centerY))
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		// 鼠标移动
+		utils.MoveMouse(outputX, outputY)
+
+		// 计算信息
+		var fps int64
+
+		cm := captureTime.Milliseconds()
+		if cm == 0 {
+			fps = 99999999999999999
+		} else {
+			fps = 1000 / cm
+		}
+
+		log.Printf("当前位置：(%2d,%2d) - 截图耗时：%5v - 帧数(每秒)：%d\n", int(outputX), int(outputY), captureTime, fps)
+		utils.BitmapSaveBMP("test.bmp")
+	}
 
 }
--- main.go
+++ main.go
@@ -30,59 +30,71 @@ var (
 	_IsWindowVisible = utils.NewProc("IsWindowVisible")
 )
 
+const (
+	Linear   = "LinearCompute"
+	Magnetic = "MagneticCompute"
+	Pid      = "PidCompute"
+	Speed    = "SpeedCompute"
+)
+
 // 获取鼠标当前移动速度
-func GetCursorPos() (currentX, currentY float64) {
+func GetCursorPos() (currentX, currentY float64, err error) {
 	if code, _, _ := _GetCursorPos.Call(uintptr(unsafe.Pointer(&currentX)), uintptr(unsafe.Pointer(&currentY))); code != 0 {
-		panic(errors.New("GetCursorPos call failed"))
+		return 0, 0, errors.New("GetCursorPos call failed")
 	}
 	return
 }
 
 // 根据鼠标当前位置移动
-func MoveMouse(dx, dy float64) {
+func MoveMouse(dx, dy float64) (err error) {
 	if code, _, _ := _MoveMouse.Call(uintptr(dx), uintptr(dy)); code != 0 {
-		panic(errors.New("MoveMouse call failed"))
+		return errors.New("MoveMouse call failed")
 	}
+	return
 }
 
 // 获取屏幕分辨率
-func GetScreenResolution() (width, height int32) {
+func GetScreenResolution() (width, height int32, err error) {
 	if code, _, _ := _GetScreenResolution.Call(uintptr(unsafe.Pointer(&width)), uintptr(unsafe.Pointer(&height))); code != 0 {
-		panic(errors.New("GetScreenResolution call failed"))
+		return 0, 0, errors.New("GetScreenResolution call failed")
 	}
 	return
 }
 
 // 线性算法初始化
-func LinearInit(t float64) {
+func LinearInit(t float64) (err error) {
 	if code, _, _ := _LinearInit.Call(uintptr(math.Float64bits(t))); code != 0 {
-		panic(errors.New("LinearInit call failed"))
+		return errors.New("LinearInit call failed")
 	}
+	return
 }
 
 // 磁吸算法初始化
-func MagneticInit(k float64) {
+func MagneticInit(k float64) (err error) {
 	if code, _, _ := _MagneticInit.Call(uintptr(math.Float64bits(k))); code != 0 {
-		panic(errors.New("MagneticInit call failed"))
+		return errors.New("MagneticInit call failed")
 	}
+	return
 }
 
 // PID算法初始化
-func PidInit(p, i, d float64) {
+func PidInit(p, i, d float64) (err error) {
 	if code, _, _ := _PidInit.Call(uintptr(math.Float64bits(p)), uintptr(math.Float64bits(i)), uintptr(math.Float64bits(d))); code != 0 {
-		panic(errors.New("MagneticInit call failed"))
+		return errors.New("MagneticInit call failed")
 	}
+	return
 }
 
 // 加速度算法初始化
-func SpeedInit(speed, deltaTime float64) {
+func SpeedInit(speed, deltaTime float64) (err error) {
 	if code, _, _ := _SpeedInit.Call(uintptr(math.Float64bits(speed)), uintptr(math.Float64bits(deltaTime))); code != 0 {
-		panic(errors.New("SpeedInit call failed"))
+		return errors.New("SpeedInit call failed")
 	}
+	return
 }
 
 // 算法
-func Compute(name string, currentX, currentY, targetX, targetY float64) (outputX, outputY float64) {
+func Compute(name string, currentX, currentY, targetX, targetY float64) (outputX, outputY float64, err error) {
 	if code, _, _ := utils.NewProc(name).Call(
 		uintptr(math.Float64bits(currentX)),
 		uintptr(math.Float64bits(currentY)),
@@ -91,13 +103,13 @@ func Compute(name string, currentX, currentY, targetX, targetY float64) (outputX
 		uintptr(unsafe.Pointer(&outputX)),
 		uintptr(unsafe.Pointer(&outputY)),
 	); code != 0 {
-		panic(errors.New("Compute call failed"))
+		return 0, 0, errors.New("Compute call failed")
 	}
 	return
 }
 
 // 捕获初始化
-func CaptureInit(hwnd, x, y, width, height int32) {
+func CaptureInit(hwnd, x, y, width, height int32) (err error) {
 	if code, _, _ := _CaptureInit.Call(
 		uintptr(hwnd),
 		uintptr(x),
@@ -105,48 +117,51 @@ func CaptureInit(hwnd, x, y, width, height int32) {
 		uintptr(width),
 		uintptr(height),
 	); code != 0 {
-		panic(errors.New("CaptureInit call failed"))
+		return errors.New("CaptureInit call failed")
 	}
+	return
 }
 
 // 捕获释放
-func CaptureRelease() {
+func CaptureRelease() (err error) {
 	if code, _, _ := _CaptureRelease.Call(); code != 0 {
-		panic(errors.New("CaptureRelease call failed"))
+		return errors.New("CaptureRelease call failed")
 	}
+	return
 }
 
 // 捕获bitmap
-func CaptureBitmap() (data []byte) {
-	if code, _, _ := _CaptureBitmap.Call(uintptr(unsafe.Pointer(&data))); code != 0 {
-		panic(errors.New("CaptureBitmap call failed"))
+func CaptureBitmap(data *[]byte) (err error) {
+	if code, _, _ := _CaptureBitmap.Call(uintptr(unsafe.Pointer(data))); code != 0 {
+		return errors.New("CaptureBitmap call failed")
 	}
 	return
 }
 
 // 保存bmp
-func BitmapSaveBMP(path string) {
+func BitmapSaveBMP(path string) (err error) {
 	if code, _, _ := _BitmapSaveBMP.Call(uintptr(unsafe.Pointer(&path))); code != 0 {
-		panic(errors.New("BitmapSaveBMP call failed"))
+		return errors.New("BitmapSaveBMP call failed")
 	}
+	return
 }
 
 // 通过窗口标题获取窗口句柄
-func GetHwndByTitle(title string) (hwnd int32) {
+func GetHwndByTitle(title string) (hwnd int32, err error) {
 	if code, _, _ := _GetHwndByTitle.Call(uintptr(unsafe.Pointer(&title)), uintptr(unsafe.Pointer(&hwnd))); code != 0 {
-		panic(errors.New("GetHwndByTitle call failed"))
+		return 0, errors.New("GetHwndByTitle call failed")
 	}
 	return
 }
 
 type Rect struct {
-	Left, Top, Right, Bottom int32
+	Width, Height int32
 }
 
 // 通过窗口句柄获取窗口RECT
-func GetWindowRect(hwnd int32) (rect Rect) {
-	if code, _, _ := _GetWindowRect.Call(uintptr(hwnd), uintptr(unsafe.Pointer(&rect.Left)), uintptr(unsafe.Pointer(&rect.Top)), uintptr(unsafe.Pointer(&rect.Right)), uintptr(unsafe.Pointer(&rect.Bottom))); code != 0 {
-		panic(errors.New("GetWindowRect call failed"))
+func GetWindowRect(hwnd int32) (rect Rect, err error) {
+	if code, _, _ := _GetWindowRect.Call(uintptr(hwnd), uintptr(unsafe.Pointer(&rect.Width)), uintptr(unsafe.Pointer(&rect.Height))); code != 0 {
+		return rect, errors.New("GetWindowRect call failed")
 	}
 	return
 }
